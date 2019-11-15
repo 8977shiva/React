7438d8a005664324423a6c0c29df48bb9ebb8631
8977shiva <b.skreddy1997@gmail.com>
2019-11-16 05:28:09 +0530

    new project

--- mosters-rolodex/src/App.js
+++ mosters-rolodex/src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters:[]    };
+      monsters:[],
+       searchField :''  
+     };
 
   }
  //life cycle method
@@ -22,6 +24,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input type='search'  placeholder='search'  
+         onChange={e=>this.setState({searchField:e.target.value})} />
          <CardList monsters={this.state.monsters}/>     
           
       
