5304ccf55b4b808af63e689fd493a4b8dd3a241e
8977shiva <b.skreddy1997@gmail.com>
2019-11-19 05:11:18 +0530

    Updates

---
+++ precache-manifest.f52a44b202fa7362ca3cdec3bcb1f5ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96975c309d0a98c46dd70a36875cab8",
+    "url": "/React/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "96ebd27a7918ce6d68c8",
+    "url": "/React/mosters-rolodex/static/css/main.b3d3eaed.chunk.css"
+  },
+  {
+    "revision": "1dd5d3ff8290eca34d19",
+    "url": "/React/mosters-rolodex/static/js/2.b744c97e.chunk.js"
+  },
+  {
+    "revision": "96ebd27a7918ce6d68c8",
+    "url": "/React/mosters-rolodex/static/js/main.af8e43ff.chunk.js"
+  },
+  {
+    "revision": "de935bf988c2ba5bb097",
+    "url": "/React/mosters-rolodex/static/js/runtime-main.811cb330.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React/precache-manifest.3e35333868e68a95cfa9b5ca9ea43c5f.js"
+  "/React/mosters-rolodex/precache-manifest.f52a44b202fa7362ca3cdec3bcb1f5ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React/mosters-rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
